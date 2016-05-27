ead026876f20a3bfe7e5a582cc2fe7039a45cd61
Adrian Buford <adrianbuford@gmail.com>
2016-05-27 09:41:12 -0500

    Update ipdropV2.sh
    Binary file (standard input) matches error fix.

--- ipdropV2.sh
+++ ipdropV2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-attackerip="$(cat /home/cowrie/cowrie/log/cowrie.log | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | tail -1)"
+attackerip="$(cat /home/cowrie/cowrie/log/cowrie.log | grep -Eoa '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | tail -1)"
 
 if grep -q $attackerip "/var/www/html/files/attackerlist.txt"; then
 
