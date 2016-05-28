57d0fd1db3d91227bee6d26146a85d46c8d99000
Adrian Buford <adrianbuford@gmail.com>
2016-05-28 11:06:10 -0500

    Update ipdropV2.sh
    eh

--- ipdropV2.sh
+++ ipdropV2.sh
@@ -1,20 +1,25 @@
+  GNU nano 2.4.2                                              File: ipdropV2.sh                                                                                                   
+
 #!/bin/bash
 
 attackerip="$(cat /home/cowrie/cowrie/log/cowrie.log | grep -Eoa '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | tail -1)"
 
-if grep -q $attackerip "/var/www/html/files/attackerlist.txt"; then
+if grep -q $attackerip /var/www/html/files/attackerlist.txt; then
 
 echo $attackerip already incorporated into firewall
 
 else
 iptables -A INPUT -s "$attackerip" -j DROP
 
-iptables -L INPUT > /var/www/html/files/attackerlist.txt
-#echo $attackerip >> /var/www/html/files/attackerlist.txt
+
+echo $attackerip >> /var/www/html/files/attackerlist.txt
 echo $attackerip added to iptables firewall
 
 fi
 
 ### echo Scripted by @drian ###
 exit 0
+
+
+
 
