3727a8dc73ef41dd158f2fc464a119f9b9cfdc45
Adrian Buford <adrianbuford@gmail.com>
2016-05-30 12:10:33 -0500

    Update ipdropV2.sh
    removed -n flag. keep getting resolved ips in config will figure out later.

--- ipdropV2.sh
+++ ipdropV2.sh
@@ -1,5 +1,3 @@
-  GNU nano 2.4.2                                           File: ipdropV2.sh                                                                                              
-
 #!/bin/bash
 
 attackerip="$(cat /home/cowrie/cowrie/log/cowrie.log | grep -Eoa '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | tail -1)"
@@ -9,7 +7,7 @@ if grep -q $attackerip /var/www/html/files/attackerlist.txt; then
 echo $attackerip already incorporated into firewall
 
 else
-/sbin/iptables -n -A INPUT -s "$attackerip" -j DROP
+/sbin/iptables -A INPUT -s "$attackerip" -j DROP
 
 
 echo $attackerip >> /var/www/html/files/attackerlist.txt
@@ -17,19 +15,15 @@ echo $attackerip added to iptables firewall
 
 fi
 
-/sbin/iptables -n -L INPUT > /var/www/html/files/attackerlistTMP.txt
+/sbin/iptables -L INPUT > /var/www/html/files/attackerlistTMP.txt
 
 if grep -q $attackerip /var/www/html/files/attackerlistTMP.txt; then
 
 echo $attackerip reverified
 
 else
-/sbin/iptables -n -A INPUT -s "$attackerip" -j DROP
+/sbin/iptables -A INPUT -s "$attackerip" -j DROP
 echo Retrying
 fi
 ### echo Scripted by @drian ###
 exit 0
-
-
-
-
